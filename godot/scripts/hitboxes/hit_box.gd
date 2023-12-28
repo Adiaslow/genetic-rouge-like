673d07f92b1d303ecc0a777049e90b857b5bb6f4
Adam Murray <63177370+Adiaslow@users.noreply.github.com>
2023-12-28 10:11:12 -0800

    Delete godot directory

--- godot/scripts/hitboxes/hit_box.gd
+++
@@ -1,9 +0,0 @@
-class_name HitBox2D
-extends Area2D
-
-@export var damage := 10
-@export var knockback := 10
-
-func _init():
-	collision_layer = 2
-	collision_mask = 0
--- godot/scripts/physics/forces/applied_force.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends Resource
-class_name AppliedForce
-
-var _this_force : Vector3
-
-func get_force(force) -> Vector3:
-	_this_force = force
-	return _this_force
